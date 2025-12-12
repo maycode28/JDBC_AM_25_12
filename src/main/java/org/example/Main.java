059c3efe217030a120fafeaa5285670f4b70d76d
maycode28 <may.code.28@gmail.com>
2025-12-12 11:48:28 +0900

    article write와 DB insert 연결

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,9 +14,10 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int lastArticleId = 0;
 
         List<Article> articles = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
 
         while (true) {
             System.out.print("명령어 > ");
@@ -23,17 +28,47 @@ public class Main {
             }
             if (cmd.equals("article write")) {
                 System.out.println("==글쓰기==");
-                int id = lastArticleId + 1;
                 System.out.print("제목 : ");
                 String title = sc.nextLine().trim();
                 System.out.print("내용 : ");
                 String body = sc.nextLine().trim();
 
-                Article article = new Article(id, title, body);
-                articles.add(article);
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
 
-                lastArticleId++;
-                System.out.println(article);
+                    String sql = "INSERT INTO article";
+                    sql += " SET regDate = NOW(),";
+                    sql += "updateDate = NOW(),";
+                    sql += "title = '"+title+"',";
+                    sql += "`body` = '"+body+"';";
+                    
+                    pstmt = conn.prepareStatement(sql);
+                    pstmt.executeUpdate();
+
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             } else if (cmd.equals("article list")) {
                 System.out.println("==목록==");
                 if (articles.size() == 0) {
