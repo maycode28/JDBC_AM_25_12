a26f49c893b34f01fd2128a5f1ccc4ce7afef9fd
maycode28 <may.code.28@gmail.com>
2025-12-12 14:25:28 +0900

    article list와DB연결, 검색 기능 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -18,6 +15,7 @@ public class Main {
         List<Article> articles = new ArrayList<>();
         Connection conn = null;
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
         while (true) {
             System.out.print("명령어 > ");
@@ -26,60 +24,85 @@ public class Main {
             if (cmd.equals("exit")) {
                 break;
             }
-            if (cmd.equals("article write")) {
-                System.out.println("==글쓰기==");
-                System.out.print("제목 : ");
-                String title = sc.nextLine().trim();
-                System.out.print("내용 : ");
-                String body = sc.nextLine().trim();
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                conn = DriverManager.getConnection(url, "root", "");
 
-                try {
-                    Class.forName("org.mariadb.jdbc.Driver");
-                    String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
-                    conn = DriverManager.getConnection(url, "root", "");
+                if (cmd.equals("article write")) {
+                    System.out.println("==글쓰기==");
+                    System.out.print("제목 : ");
+                    String title = sc.nextLine().trim();
+                    System.out.print("내용 : ");
+                    String body = sc.nextLine().trim();
+
 
                     String sql = "INSERT INTO article";
                     sql += " SET regDate = NOW(),";
                     sql += "updateDate = NOW(),";
-                    sql += "title = '"+title+"',";
-                    sql += "`body` = '"+body+"';";
+                    sql += "title = '" + title + "',";
+                    sql += "`body` = '" + body + "';";
 
                     pstmt = conn.prepareStatement(sql);
                     pstmt.executeUpdate();
 
+                } else if (cmd.startsWith("article list")) {
+                    String sql = "SELECT *";
+                    sql += " FROM article";
+                    sql += " ORDER BY id DESC;";
 
-                } catch (ClassNotFoundException e) {
-                    System.out.println("드라이버 로딩 실패" + e);
+                    pstmt = conn.prepareStatement(sql);
+                    rs = pstmt.executeQuery();
+                    if (!rs.next()) {
+                        System.out.println("게시글 없음");
+                        continue;
+                    }
+                    String searchKeyword = cmd.substring("article list".length()).trim();
+
+
+                    System.out.println("==목록==");
+                    System.out.println("   번호    /    제목");
+                    if (searchKeyword.isEmpty()) {
+                        rs.previous();
+                        while (rs.next()) {
+                            System.out.printf("%d    /   %s\n", rs.getInt("id"), rs.getString("title"));
+                        }
+                    }else{
+                        sql = "SELECT *";
+                        sql += " FROM article";
+                        sql += " WHERE title LIKE '%" + searchKeyword + "%'";
+                        sql += " ORDER BY id DESC;";
+                        pstmt = conn.prepareStatement(sql);
+                        rs = pstmt.executeQuery();
+                        while (rs.next()) {
+                            System.out.printf("%d    /   %s\n", rs.getInt("id"), rs.getString("title"));
+                        }
+                    }
+
+                }
+
+
+            } catch (ClassNotFoundException e) {
+                System.out.println("드라이버 로딩 실패" + e);
+            } catch (SQLException e) {
+                System.out.println("에러 : " + e);
+            } finally {
+                try {
+                    if (conn != null && !conn.isClosed()) {
+                        conn.close();
+                    }
                 } catch (SQLException e) {
-                    System.out.println("에러 : " + e);
-                } finally {
-                    try {
-                        if (conn != null && !conn.isClosed()) {
-                            conn.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        if (pstmt != null && !pstmt.isClosed()) {
-                            pstmt.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
+                    e.printStackTrace();
                 }
-
-            } else if (cmd.equals("article list")) {
-                System.out.println("==목록==");
-                if (articles.size() == 0) {
-                    System.out.println("게시글 없음");
-                    continue;
-                }
-                System.out.println("   번호    /    제목");
-                for (Article article : articles) {
-                    System.out.printf("   %d    /   %s\n", article.getId(), article.getTitle());
+                try {
+                    if (pstmt != null && !pstmt.isClosed()) {
+                        pstmt.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
                 }
             }
+
         }
 
         System.out.println("==프로그램 종료==");
